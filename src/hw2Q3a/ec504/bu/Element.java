a7aff8c09c2903fba9e69d7fe82bd11ac4a4c641
zhswatson <zhswatson@gmail.com>
2014-02-25 13:51:10 -0500

    initial commit after project creation

---
+++ src/hw2Q3a/ec504/bu/Element.java
@@ -0,0 +1,5 @@
+package hw2Q3a.ec504.bu;
+
+public class Element {
+
+}
---
+++ src/hw2Q3a/ec504/bu/PriorityQueueExample.java
@@ -0,0 +1,11 @@
+package hw2Q3a.ec504.bu;
+
+public class PriorityQueueExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("haha");
+	}
+
+}
